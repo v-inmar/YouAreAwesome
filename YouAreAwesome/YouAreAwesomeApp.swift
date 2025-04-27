16b8e6c7c6f481d79a89438fffe9754be0f14b5a
v-inmar <vinmar.tipay@outlook.com>
2025-04-27 21:24:55 +0100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Vinmar Tipay on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
