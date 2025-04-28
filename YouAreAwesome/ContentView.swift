749afd3ae1b46cb3ae83317c55e0219de5920a07
v-inmar <vinmar.tipay@outlook.com>
2025-04-28 21:00:55 +0100

    Replaced text message for testing

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+            Text("I am an App Developer!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
